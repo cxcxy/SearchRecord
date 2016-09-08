128ac035b1557a9ae089dc1fd3e0719fa69e4526
陈旭 <897939354@qq.com>
2016-09-08 15:10:28 +0800

    Initial Commit

---
+++ SearchRecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchRecord
+//
+//  Created by 陈旭 on 16/9/8.
+//  Copyright © 2016年 陈旭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
